fb69804b09fe0143fd612485099dceca1b4a40de
xfrnk2 <xfrnk2@gmail.com>
2023-07-08 14:23:31 +0900

    feat: clustering module

--- GoogleMap copy.js
+++ GoogleMap copy.js
@@ -1,7 +1,7 @@
-import { PROVIDER_GOOGLE, Marker } from "react-native-maps";
-//import MapView from "react-native-maps";
-import MapView from "react-native-map-clustering";
+import { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
+import MapView from "react-native-maps";
 import { StyleSheet, View, Button } from "react-native";
+import ClusteredMapView from "react-native-maps-super-cluster";
 
 import React, { useState, useEffect } from "react";
 import CustomMarker from "./components/markers/CustomMarker";
@@ -13,6 +13,13 @@ const GoogleMap = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [stores, setStores] = useState([]);
+  const [region, setRegion] = useState({
+    latitude: 37.5665,
+    longitude: 126.978,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  const [ggroupedMarkers, setGgroupedMarkers] = useState({});
 
   useEffect(() => {
     (async () => {
@@ -24,12 +31,16 @@ const GoogleMap = () => {
 
       let currentLocation = await Location.getCurrentPositionAsync({});
       setLocation(currentLocation);
+      console.log(
+        "location...0000000000000000000000000000000000000000000000",
+        location
+      );
     })();
 
     (async () => {
       try {
         const response = await axios.get(
-          "https://d342-219-255-155-95.ngrok-free.app/store",
+          "https://1cb3-219-255-155-95.ngrok-free.app/store",
           (headers = {
             // "Content-Type": "multipart/form-data",
             Accept: "application/json",
@@ -38,8 +49,25 @@ const GoogleMap = () => {
 
         const data = response.data;
         console.log(data);
+        console.log(
+          "999999999999999999999999999999999그..전체크기",
+          data.length
+        );
         setStores(data);
         console.log(stores);
+
+        // 클러스터링을 위해 구 이름으로 마커를 그룹화
+        // const groupedMarkers = {};
+        // stores.forEach((marker) => {
+        //   // const { title } = marker;
+        //   if (groupedMarkers["abc"]) {
+        //     groupedMarkers["abc"].push(marker);
+        //   } else {
+        //     groupedMarkers["abc"] = [marker];
+        //   }
+        // });
+        // console.log("111111111111", groupedMarkers);
+        // setGgroupedMarkers(groupedMarkers);
       } catch (err) {
         console.log(err);
         console.log(err.name);
@@ -70,7 +98,11 @@ const GoogleMap = () => {
     console.log(location.coords.longitude);
     lag = location.coords.latitude;
     log = location.coords.longitude;
-
+    // setRegion({
+    //   ...region,
+    //   latitude: location.coords.latitude,
+    //   longitude: location.coords.longitude,
+    // });
     // const markerComponents = stores.map((marker, index) => (
     //   <CustomMarker
     //     key={index} // 고유한 키 값 설정
@@ -85,6 +117,44 @@ const GoogleMap = () => {
     // ));
   }
 
+  // 줌 레벨에 따라 클러스터 또는 개별 마커를 렌더링
+  // const renderMarkers = () => {
+  //   const zoomLevel = Math.round(Math.log2(360 / region.longitudeDelta)) + 1;
+
+  //   if (zoomLevel < 12) {
+  //     // 클러스터링된 마커만 표시
+  //     return Object.keys(ggroupedMarkers).map((title) => {
+  //       const clusteredMarkers = ggroupedMarkers[title];
+  //       const clusterCoordinate = {
+  //         latitude: clusteredMarkers[0].lat,
+  //         longitude: clusteredMarkers[0].lon,
+  //       };
+
+  //       return (
+  //         <CustomMarker
+  //           key={title}
+  //           coordinate={clusterCoordinate}
+  //           id={`${title} (${clusteredMarkers.length})`}
+  //           nM="Zoom in to see individual markers"
+  //         />
+  //       );
+  //     });
+  //   } else {
+  //     // 개별 마커 표시
+  //     return stores.map((marker) => (
+  //       <CustomMarker
+  //         key={marker.id}
+  //         nM={marker.nM}
+  //         coordinate={{
+  //           latitude: marker.lat,
+  //           longitude: marker.lon,
+  //         }}
+  //         id={marker.title}
+  //       />
+  //     ));
+  //   }
+  // };
+
   return (
     <View style={styles.screen}>
       {lag !== 0 && log !== 0 && (
@@ -97,7 +167,6 @@ const GoogleMap = () => {
             longitudeDelta: 1,
           }}
           provider={PROVIDER_GOOGLE}
-          clusterColor="#000000"
         >
           {/* <Marker
             coordinate={{
@@ -113,7 +182,9 @@ const GoogleMap = () => {
             anchor={{ x: 0.5, y: 1 }}
           ></CustomMarker> */}
           {/* <View>{stores}</View>; */}
-          {stores.map((marker, index) => (
+
+          {/* {renderMarkers()} */}
+          {/* {stores.map((marker, index) => (
             <CustomMarker
               key={index} // 고유한 키 값 설정
               coordinate={{
@@ -124,7 +195,7 @@ const GoogleMap = () => {
               nM={marker.nM}
               anchor={{ x: 0.5, y: 1 }}
             />
-          ))}
+          ))} */}
         </MapView>
       )}
     </View>
